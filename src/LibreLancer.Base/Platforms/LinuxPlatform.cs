563155188351cb49ebe7207ffa05e341162fc4b5
CallumDev <callum.mcging@gmail.com>
2016-03-13 17:00:12 +1030

    Implement Linux System Fonts

--- src/LibreLancer.Base/Platforms/LinuxPlatform.cs
+++ src/LibreLancer.Base/Platforms/LinuxPlatform.cs
@@ -1,9 +1,17 @@
 ﻿using System;
 using SharpFont;
+using FontConfigSharp;
 namespace LibreLancer.Platforms
 {
 	class LinuxPlatform : IPlatform
 	{
+		FcConfig fcconfig;
+
+		public LinuxPlatform()
+		{
+			fcconfig = Fc.InitLoadConfigAndFonts ();
+		}
+
 		public bool IsDirCaseSensitive (string directory)
 		{
 			return true;
@@ -11,7 +19,19 @@ namespace LibreLancer.Platforms
 
 		public Face LoadSystemFace (Library library, string face)
 		{
-			throw new NotImplementedException ();
+			string file = null;
+			using (var pat = FcPattern.FromFamilyName (face)) {
+				pat.ConfigSubstitute (fcconfig, FcMatchKind.Pattern);
+				pat.DefaultSubstitute ();
+				FcResult result;
+				using (var font = pat.Match (fcconfig, out result)) {
+					if (font.GetString (Fc.FC_FILE, 0, ref file) == FcResult.Match) {
+						return new Face (library, file);
+					}
+				}
+			}
+			//This shouldn't be thrown since fontconfig substitutes, but have this just in case
+			throw new Exception ("Font not found: " + face);
 		}
 	}
 }
