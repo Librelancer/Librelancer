8271261ed25a93793ff4b3dd2cb3ac4855313272
CallumDev <callum.mcging@gmail.com>
2017-01-10 00:20:15 +1030

    Font style lookup on Win32

--- src/LibreLancer.Base/Platforms/Win32Platform.cs
+++ src/LibreLancer.Base/Platforms/Win32Platform.cs
@@ -27,11 +27,18 @@ namespace LibreLancer.Platforms
 
 		public Face LoadSystemFace (Library library, string face, ref FontStyles style)
 		{
-			byte[] buffer;
+			int weight = GDI.FW_REGULAR;
+			uint fdwItalic = 0;
+			//Map style
+			if ((style & FontStyles.Bold) == FontStyles.Bold)
+				weight = GDI.FW_BOLD;
+			if ((style & FontStyles.Italic) == FontStyles.Italic)
+				fdwItalic = 1;
 			//Get font data from GDI
+			byte[] buffer;
 			unsafe {
-				var hfont = GDI.CreateFont (0, 0, 0, 0, GDI.FW_REGULAR,
-					0, 0, 0, GDI.DEFAULT_CHARSET, GDI.OUT_OUTLINE_PRECIS,
+				var hfont = GDI.CreateFont (0, 0, 0, 0, weight,
+					fdwItalic, 0, 0, GDI.DEFAULT_CHARSET, GDI.OUT_OUTLINE_PRECIS,
 					GDI.CLIP_DEFAULT_PRECIS, GDI.DEFAULT_QUALITY,
 					GDI.DEFAULT_PITCH, face);
 				//get data
@@ -46,10 +53,16 @@ namespace LibreLancer.Platforms
 				//delete font
 				GDI.DeleteObject (hfont);
 			}
-			//TODO: Implement style matching on Windows
-			style = FontStyles.Regular;
 			//create font object
-			return new Face(library, buffer,0);
+			//TODO: Handle multi-face data on Windows. (Is that a thing?)
+			var fc = new Face(library, buffer,0);
+			//Extract style
+			var fs = FontStyles.Regular;
+			if ((fc.StyleFlags & StyleFlags.Bold) != 0) fs |= FontStyles.Bold;
+			if ((fc.StyleFlags & StyleFlags.Italic) != 0) fs |= FontStyles.Italic;
+			style = fs;
+			//return font
+			return fc;
 		}
 	}
 }
