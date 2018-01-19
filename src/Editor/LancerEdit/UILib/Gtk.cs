0cb30bb53493a3c208c0bd3b3328debf17066bc9
CallumDev <callum.mcging@gmail.com>
2018-01-19 17:21:23 +1030

    Editor: Fix Linux sonames

--- src/Editor/LancerEdit/UILib/Gtk.cs
+++ src/Editor/LancerEdit/UILib/Gtk.cs
@@ -4,7 +4,9 @@ namespace LancerEdit
 {
 	static class Gtk
 	{
-		[DllImport("libgtk-3.so")]
+		public const string LIB="libgtk-3.so.0";
+
+		[DllImport(LIB)]
 		public static extern bool gtk_init_check(IntPtr argc, IntPtr argv);
 
 		public const int GTK_FILE_CHOOSER_ACTION_OPEN = 0;
@@ -14,32 +16,32 @@ namespace LancerEdit
 		public const int GTK_RESPONSE_CANCEL = -6;
 		public const int GTK_RESPONSE_ACCEPT = -3;
 
-		[DllImport("libgtk-3.so")]
+		[DllImport(LIB)]
 		public static extern IntPtr gtk_file_chooser_dialog_new(
 			[MarshalAs(UnmanagedType.LPStr)]string title,
 			IntPtr parent,
 			int action,
 			IntPtr ignore);
 
-		[DllImport("libgtk-3.so")]
+		[DllImport(LIB)]
 		public static extern int gtk_dialog_run(IntPtr dialog);
 
-		[DllImport("libgtk-3.so")]
+		[DllImport(LIB)]
 		public static extern bool gtk_events_pending();
 
-		[DllImport("libgtk-3.so")]
+		[DllImport(LIB)]
 		public static extern void gtk_main_iteration();
 
-		[DllImport("libgtk-3.so")]
+		[DllImport(LIB)]
 		public static extern void gtk_widget_destroy(IntPtr widget);
 
-		[DllImport("libgtk-3.so")]
+		[DllImport(LIB)]
 		public static extern void gtk_dialog_add_button(IntPtr dialog, [MarshalAs(UnmanagedType.LPStr)]string button_text, int response_id);
 
-		[DllImport("libgtk-3.so")]
+		[DllImport(LIB)]
 		public static extern IntPtr gtk_file_chooser_get_filename(IntPtr chooser);
 
-		[DllImport("libgtk-3.so")]
+		[DllImport(LIB)]
 		public static extern void gtk_window_set_keep_above(IntPtr window, bool value);
 	}
 }
