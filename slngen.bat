3b457869375580c471d8ab5c4369bebef8174baa
CallumDev <callum.mcging@gmail.com>
2018-02-08 00:08:10 +1030

    slngen: Ensure release mode on Windows

--- slngen.bat
+++ slngen.bat
@@ -4,10 +4,10 @@ IF [%1]==[/?] (GOTO USAGE)
 IF EXIST slngen\slngen\bin\Debug\slngen.exe (GOTO RUNCMD)
 :REBUILD
 cd slngen
-%WINDIR%\\Microsoft.NET\Framework\v4.0.30319\msbuild.exe
+%WINDIR%\\Microsoft.NET\Framework\v4.0.30319\msbuild.exe /p:Configuration=Release
 cd ..
 :RUNCMD
-slngen\slngen\bin\Debug\slngen.exe
+slngen\slngen\bin\Release\slngen.exe
 EXIT
 :USAGE
 echo Usage: %0 [/rebuild]
