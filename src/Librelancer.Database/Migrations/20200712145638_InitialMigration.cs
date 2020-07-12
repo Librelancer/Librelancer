d2e894afb0f47e98ef7dad6ef9b4d007a6aeb55c
Lazrius <lazdisco@gmail.com>
2020-07-12 16:01:59 +0100

    Create initial db migration

---
+++ src/Librelancer.Database/Migrations/20200712145638_InitialMigration.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LibreLancer.Database.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Accounts",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CreationDate = table.Column<DateTime>(nullable: false),
+                    UpdateDate = table.Column<DateTime>(nullable: true),
+                    AccountIdentifier = table.Column<Guid>(nullable: false),
+                    LastLogin = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Accounts", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Characters",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CreationDate = table.Column<DateTime>(nullable: false),
+                    UpdateDate = table.Column<DateTime>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    Rank = table.Column<uint>(nullable: false),
+                    Money = table.Column<decimal>(nullable: false),
+                    Voice = table.Column<string>(nullable: true),
+                    Costume = table.Column<string>(nullable: true),
+                    ComCostume = table.Column<string>(nullable: true),
+                    System = table.Column<string>(nullable: true),
+                    Base = table.Column<string>(nullable: true),
+                    Ship = table.Column<string>(nullable: true),
+                    Affiliation = table.Column<string>(nullable: true),
+                    AffiliationLocked = table.Column<bool>(nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    FightersKilled = table.Column<ulong>(nullable: false),
+                    TransportsKilled = table.Column<ulong>(nullable: false),
+                    CapitalKills = table.Column<ulong>(nullable: false),
+                    PlayersKilled = table.Column<ulong>(nullable: false),
+                    MissionsCompleted = table.Column<ulong>(nullable: false),
+                    MissionsFailed = table.Column<ulong>(nullable: false),
+                    AccountId = table.Column<long>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Characters", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Characters_Accounts_AccountId",
+                        column: x => x.AccountId,
+                        principalTable: "Accounts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CargoItem",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CreationDate = table.Column<DateTime>(nullable: false),
+                    UpdateDate = table.Column<DateTime>(nullable: true),
+                    ItemName = table.Column<string>(nullable: true),
+                    ItemCount = table.Column<ulong>(nullable: false),
+                    IsMissionItem = table.Column<bool>(nullable: false),
+                    CharacterId = table.Column<long>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CargoItem", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CargoItem_Characters_CharacterId",
+                        column: x => x.CharacterId,
+                        principalTable: "Characters",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EquipmentEntity",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CreationDate = table.Column<DateTime>(nullable: false),
+                    UpdateDate = table.Column<DateTime>(nullable: true),
+                    EquipmentNickname = table.Column<string>(nullable: true),
+                    EquipmentHardpoint = table.Column<string>(nullable: true),
+                    CharacterId = table.Column<long>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EquipmentEntity", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_EquipmentEntity_Characters_CharacterId",
+                        column: x => x.CharacterId,
+                        principalTable: "Characters",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Reputation",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CreationDate = table.Column<DateTime>(nullable: false),
+                    UpdateDate = table.Column<DateTime>(nullable: true),
+                    ReputationValue = table.Column<float>(nullable: false),
+                    RepGroup = table.Column<string>(nullable: true),
+                    CharacterId = table.Column<long>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reputation", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Reputation_Characters_CharacterId",
+                        column: x => x.CharacterId,
+                        principalTable: "Characters",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "VisitEntry",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CreationDate = table.Column<DateTime>(nullable: false),
+                    UpdateDate = table.Column<DateTime>(nullable: true),
+                    VisitValue = table.Column<int>(nullable: false),
+                    SolarNickname = table.Column<string>(nullable: true),
+                    CharacterId = table.Column<long>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VisitEntry", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_VisitEntry_Characters_CharacterId",
+                        column: x => x.CharacterId,
+                        principalTable: "Characters",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Accounts_AccountIdentifier",
+                table: "Accounts",
+                column: "AccountIdentifier");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CargoItem_CharacterId",
+                table: "CargoItem",
+                column: "CharacterId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Characters_AccountId",
+                table: "Characters",
+                column: "AccountId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EquipmentEntity_CharacterId",
+                table: "EquipmentEntity",
+                column: "CharacterId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reputation_CharacterId",
+                table: "Reputation",
+                column: "CharacterId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VisitEntry_CharacterId",
+                table: "VisitEntry",
+                column: "CharacterId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CargoItem");
+
+            migrationBuilder.DropTable(
+                name: "EquipmentEntity");
+
+            migrationBuilder.DropTable(
+                name: "Reputation");
+
+            migrationBuilder.DropTable(
+                name: "VisitEntry");
+
+            migrationBuilder.DropTable(
+                name: "Characters");
+
+            migrationBuilder.DropTable(
+                name: "Accounts");
+        }
+    }
+}
