7833706c1fe33c0f1552673f98edbb8c29a3c7dc
CallumDev <callum.mcging@gmail.com>
2022-06-10 02:21:02 +0930

    Fix typo in gen.py

--- src/LibreLancer/Net/Protocol/gen.py
+++ src/LibreLancer/Net/Protocol/gen.py
@@ -1,4 +1,4 @@
-+ speed# Run with python3 gen.py
+# Run with python3 gen.py
 # MIT License - Copyright (c) Callum McGing
 # This file is subject to the terms and conditions defined in
 # LICENSE, which is part of this source code package
