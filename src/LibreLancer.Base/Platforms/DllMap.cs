1670ea9aad23c9d3ad78403bf1134045e7f7eda1
CallumDev <callum.mcging@gmail.com>
2022-08-29 04:12:32 +0930

    DllMap: Don't load the same XML file multiple times

--- src/LibreLancer.Base/Platforms/DllMap.cs
+++ src/LibreLancer.Base/Platforms/DllMap.cs
@@ -15,46 +15,38 @@ namespace LibreLancer
     //Emulate mono's DllImport behaviour on Linux
     static class DllMap
     {
+        private static Dictionary<string, string> libs = new Dictionary<string, string>();
+        
         public static void Register(Assembly assembly)
         {
+            lock (libs)
+            {
+                string xmlPath = assembly.Location + ".config";
+                if (File.Exists(xmlPath))
+                {
+                    foreach (var el in XElement.Load(xmlPath).Elements("dllmap"))
+                    {
+                        if (!el.Attribute("os").ToString().Contains("linux", StringComparison.OrdinalIgnoreCase))
+                            continue;
+                        string oldLib = el.Attribute("dll").Value;
+                        string newLib = el.Attribute("target").Value;
+                        if (string.IsNullOrWhiteSpace(oldLib) || string.IsNullOrWhiteSpace(newLib))
+                            continue;
+                        libs[oldLib] = newLib;
+                    }
+                }
+            }
             NativeLibrary.SetDllImportResolver(assembly, MapAndLoad);
         }
         private static IntPtr MapAndLoad(string libraryName, Assembly assembly,
             DllImportSearchPath? dllImportSearchPath)
         {
             string mappedName = null;
-            mappedName = MapLibraryName(assembly.Location, libraryName, out mappedName) ? mappedName : libraryName;
+            lock (libs) {
+                mappedName = libs.TryGetValue(libraryName, out mappedName) ? mappedName : libraryName;
+            }
             return NativeLibrary.Load(mappedName, assembly, dllImportSearchPath);
         }
-
-        static Dictionary<string, string> mapped = new Dictionary<string, string>();
-        private static bool MapLibraryName(string assemblyLocation, string originalLibName, out string mappedLibName)
-        {
-            mappedLibName = null;
-            if (mapped.TryGetValue(originalLibName, out mappedLibName))
-                return true;
-            string xmlPath = assemblyLocation + ".config";
-            if (!File.Exists(xmlPath))
-                return TryFind(assemblyLocation, originalLibName, out mappedLibName);
-            FLLog.Debug("DllMap", xmlPath);
-            XElement root = XElement.Load(xmlPath);
-            var map =
-                (from el in root.Elements("dllmap")
-                    where ((string) el.Attribute("dll") == originalLibName) && ((string) el.Attribute("os") == "linux")
-                    select el).SingleOrDefault();
-            if (map != null)
-            {
-                mappedLibName = map.Attribute("target").Value;
-                mapped.Add(originalLibName, mappedLibName);
-            }
-
-            return (mappedLibName != null);
-        }
-
-        static bool TryFind(string assemblyLocation, string originalLibName, out string mappedLibName)
-        {
-            mappedLibName = null;
-            return false;
-        }
+        
     }
 }
