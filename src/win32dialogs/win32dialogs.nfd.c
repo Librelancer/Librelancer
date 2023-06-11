f42a3ab51661fbb0e344286766f44c55e84d4f08
CallumDev <callum.mcging@gmail.com>
2023-06-11 09:57:06 +0930

    Fix win32dialogs and bulletc name under mingw

--- src/win32dialogs/win32dialogs.nfd.c
+++ src/win32dialogs/win32dialogs.nfd.c
@@ -1,17 +1,46 @@
 #include "win32dialogs.h"
 #include <nfd.h>
+#include "combaseapi.h"
+#include <stdlib.h>
+#include <string.h>
+
+// Copy from the allocator used from nfd to something .NET can free
+void CopyToWin32Memory(char *input, char **output)
+{
+    if(input) 
+    {
+        int len = strlen(input);
+        *output = (char*)CoTaskMemAlloc(len + 1);
+        (*output)[len] = 0;
+        memcpy(*output, input, len);
+        free(input);
+    } 
+    else 
+    {
+        *output = (char*)0;
+    }
+}
 
 DLGAPI Win32OpenDialog(const char *filters, const char *defaultPath, char **outPath)
 {
-	return NFD_OpenDialog(filters, defaultPath, outPath) == NFD_OKAY;
+    char *res;
+	int retval = NFD_OpenDialog(filters, defaultPath, &res) == NFD_OKAY;
+	CopyToWin32Memory(res, outPath);
+	return retval;
 }
 
 DLGAPI Win32SaveDialog(const char *filters, const char *defaultPath, char **outPath)
 {
-	return NFD_SaveDialog(filters, defaultPath, outPath) == NFD_OKAY;
+    char *res;
+	int retval =  NFD_SaveDialog(filters, defaultPath, &res) == NFD_OKAY;
+	CopyToWin32Memory(res, outPath);
+	return retval;
 }
 
 DLGAPI Win32PickFolder(const char *defaultPath, char **outPath)
 {
-	return NFD_PickFolder(defaultPath, outPath) == NFD_OKAY;
+    char *res;
+	int retval = NFD_PickFolder(defaultPath, &res) == NFD_OKAY;
+	CopyToWin32Memory(res, outPath);
+	return retval;
 }
