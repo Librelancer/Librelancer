85802c852fea5a570d3005c703bf7bacac39ad31
Jason Leonard <jason@intensepurple.co.uk>
2024-03-14 19:13:06 +0000

    Allow ini parsers to determine if they can parse the data stream

--- src/LibreLancer.Data/Ini/IniFile.cs
+++ src/LibreLancer.Data/Ini/IniFile.cs
@@ -5,41 +5,19 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Text;
-using LibreLancer.Data;
 using LibreLancer.Data.IO;
 
 namespace LibreLancer.Ini
 {
 	public abstract partial class IniFile
 	{
-        private const string Bini = "BINI";
-
-        private static string DetermineFileType(Stream stream)
-        {
-            stream.Seek(0, SeekOrigin.Begin);
-            byte[] buffer = new byte[4];
-            stream.Read(buffer, 0, 4);
-            var fileType = Encoding.ASCII.GetString(buffer);
-            stream.Seek(0, SeekOrigin.Begin);
-            return fileType;
-        }
-
         public static IEnumerable<Section> ParseFile(string path, Stream stream, bool preparse = true, bool allowmaps = false)
         {
             if (string.IsNullOrEmpty(path)) path = "[Memory]";
-            
-            var fileType = DetermineFileType(stream);
 
-            IIniParser parser = null;
-            if (fileType == Bini) 
+            IIniParser parser = new BinaryIniParser();
+            if (!parser.CanParse(stream)) 
             {
-                // Binary Ini
-                parser = new BinaryIniParser();
-            }
-            else 
-            {
-                // Text Ini
                 parser = new TextIniParser();
             }
             return parser.ParseIniFile(path, stream, preparse, allowmaps);
