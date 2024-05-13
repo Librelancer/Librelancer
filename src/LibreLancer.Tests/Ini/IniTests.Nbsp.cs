c1c630507dd7757e100072579d1ddab53c3075d5
Jason Leonard <jason@intensepurple.co.uk>
2024-05-13 11:28:23 +0100

    Fix for unicode nbsp character when compiler doesn't read csharp file as windows-1252

--- src/LibreLancer.Tests/Ini/IniTests.Nbsp.cs
+++ src/LibreLancer.Tests/Ini/IniTests.Nbsp.cs
@@ -15,16 +15,16 @@ public partial class IniTests
     public void NbspTest()
     {
         var ini = ParseFile(TestAsset.Open<IniTests>("Nbsp.ini"), false, false).ToList();
-        ini[0].Name.Should().Be("nbsp        ");
-        ini[0][0].Name.Should().Be("locked_         gate        ");
+        ini[0].Name.Should().Be("nbsp\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0");
+        ini[0][0].Name.Should().Be("locked_\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0 gate\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0");
         ini[0][0].Should().HaveCount(1);
         ini[0][0][0].ToString().Should().Be("2926089285");
         ini[0][0][0].ToBoolean().Should().Be(false);
         ini[0][0][0].ToInt32().Should().Be(-1368878011);
         ini[0][0][0].ToSingle().Should().Be((float)2926089285.000000);
         ini[0].Count.Should().Be(1);
-        ini[1].Name.Should().Be("        nbsp        ");
-        ini[1][0].Name.Should().Be("lock_         gate        ");
+        ini[1].Name.Should().Be("\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0nbsp\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0");
+        ini[1][0].Name.Should().Be("lock_\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0 gate\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0");
         ini[1][0].Should().HaveCount(0);
         ini[1].Count.Should().Be(1);
         ini.Count.Should().Be(2);
