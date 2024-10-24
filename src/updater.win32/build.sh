832dd15b3e3203172b8e02f395d1c892eac44d66
CallumDev <callum.mcging@gmail.com>
2024-10-24 13:11:43 +1030

    win: Properly sign updater.exe

--- src/updater.win32/build.sh
+++ src/updater.win32/build.sh
@@ -2,5 +2,5 @@
 
 # Keyfile required
 # We use different output filename to satisfy AV
-mcs -keyfile:"$1" -delaysign -win32icon:../Editor/LancerEdit/reactor.ico -out:LibrelancerUpdate.exe -target:winexe -sdk:4.5 -r:System.Windows.Forms.dll -r:System.Drawing.dll -r:System.IO.Compression.dll -r:System.dll -r:System.IO.Compression.FileSystem.dll Program.cs
+mcs -keyfile:"$1" -win32icon:../Editor/LancerEdit/reactor.ico -out:LibrelancerUpdate.exe -target:winexe -sdk:4.5 -r:System.Windows.Forms.dll -r:System.Drawing.dll -r:System.IO.Compression.dll -r:System.dll -r:System.IO.Compression.FileSystem.dll Program.cs
 mv LibrelancerUpdate.exe updater.exe
