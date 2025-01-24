19b6ae63a622ca6c21cbccfbcb5e235cbc591144
Lazrius <lazdisco@gmail.com>
2025-01-24 12:40:50 +0000

    feat: convert frc file into ResourceDll with tests

---
+++ src/Editor/LibreLancer.ContentEdit/Frc/FrcCompiler.cs
@@ -0,0 +1,445 @@
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using LibreLancer.ContentEdit.RandomMissions;
+using LibreLancer.Dll;
+
+namespace LibreLancer.ContentEdit.Frc;
+
+public class FrcCompiler
+{
+    private enum State
+    {
+        Start,
+        String,
+        StringIds,
+        Info,
+        InfoIds,
+    }
+
+    private enum SpecialCharacterState
+    {
+        None,
+        Color,
+        FontNumber,
+        Pos,
+        Unicode
+    }
+
+    [SuppressMessage("ReSharper", "UnusedMember.Local")]
+    private enum AvailableColour
+    {
+        Gray,
+        Blue,
+        Green,
+        Aqua,
+        Red,
+        Fuchsia,
+        Yellow,
+        White
+    }
+
+    private int currentIdsNumber = -1;
+    private string currentString = string.Empty;
+
+    private bool skipWhitespace;
+    private char lastSymbol = '\0';
+    private string currentSpecialString = string.Empty;
+    private bool lastCharWasNewLine;
+    private State state = State.Start;
+    private SpecialCharacterState specialCharacterState = SpecialCharacterState.None;
+
+    private static bool IsSpace(char c) => c is ' ' or '\t';
+
+    public const string InfocardStart = "<?xml version=\"1.0\" encoding=\"UTF-16\"?><RDL><PUSH/>";
+    public const string InfocardEnd = "<POP/></RDL>";
+    public ResourceDll Compile(string text, string source, int resourceIndex = -1)
+    {
+        if (string.IsNullOrWhiteSpace(text))
+        {
+            throw new ArgumentNullException(nameof(text));
+        }
+
+        ResourceDll res = new();
+
+        var reader = new FrcReader(text, source);
+
+        var ch = (char)reader.Current();
+        state = ch switch
+        {
+            'S' => State.StringIds,
+            'H' or 'I' => State.InfoIds,
+            _ => throw new CompileErrorException(source, reader.Column, reader.Line,
+                $"Unexpected character '{ch}'. Expected S, H/I")
+        };
+
+        while (reader.Current() != -1)
+        {
+            if (!Advance(state is not State.String and not State.Info || lastCharWasNewLine || skipWhitespace))
+            {
+                break;
+            }
+
+            if (skipWhitespace)
+            {
+                skipWhitespace = false;
+                lastSymbol = ' ';
+            }
+
+            ch = (char)reader.Current();
+            if (ch == '\r')
+            {
+                continue;
+            }
+
+            if (reader.Column is 1 && char.IsAsciiLetterUpper(ch))
+            {
+                // Line ended without finishing a special character
+                if (specialCharacterState != SpecialCharacterState.None)
+                {
+                    var oldCh = ch;
+                    ch = '\0';
+                    HandleSpecialCharacter();
+                    ch = oldCh;
+                }
+
+                switch (state)
+                {
+                    // We are changing the state, lets finish what we were doing with the previous state
+                    case State.String:
+                        res.Strings[currentIdsNumber] = currentString;
+                        break;
+                    case State.Info:
+                        res.Infocards[currentIdsNumber] = InfocardStart + currentString + InfocardEnd;
+                        break;
+                    case State.StringIds:
+                    case State.InfoIds:
+                    {
+                        throw new CompileErrorException(source, reader.Column, reader.Line,
+                            $"Unexpected character '{ch}'. Was expecting a {(state is State.StringIds ? "string" : "infocard")}.");
+                    }
+                    case State.Start:
+                    default:
+                        break;
+                }
+
+                state = ch switch
+                {
+                    'S' => State.StringIds,
+                    'H' or 'I' => State.InfoIds,
+                    _ => throw new CompileErrorException(source, reader.Column, reader.Line,
+                        $"Unexpected character '{ch}'. Expected S, H/I")
+                };
+
+                currentString = string.Empty;
+                currentIdsNumber = -1;
+                lastSymbol = ch;
+                lastCharWasNewLine = false;
+                continue;
+            }
+
+            if (state is State.StringIds or State.InfoIds)
+            {
+                if (!HandleIds())
+                {
+                    continue;
+                }
+
+                state = state is State.StringIds ? State.String : State.Info;
+            }
+
+            if (ch is '\n')
+            {
+                // If we end our string with a backslash, don't add a new line to the string
+                if (lastSymbol is not '\\')
+                {
+                    lastCharWasNewLine = true;
+                }
+                else
+                {
+                    skipWhitespace = true;
+                }
+
+                continue;
+            }
+
+            if (lastCharWasNewLine)
+            {
+                if (currentString.Length is not 0)
+                {
+                    currentString += state is State.String ? "\n" : "<PARA/>";
+                }
+
+                lastCharWasNewLine = false;
+            }
+
+            switch (lastSymbol)
+            {
+                case '\\' when ch is ' ':
+                    if (currentString.Length is 0)
+                    {
+                        currentString += ch;
+                    }
+
+                    lastSymbol = '\0';
+                    break;
+                case '\\':
+                    if (HandleSpecialCharacter())
+                    {
+                        if (ch is not '\\' and not '\n')
+                        {
+                            currentString += ch;
+                        }
+
+                        lastSymbol = ch;
+                    }
+                    break;
+                default:
+                    if (ch is not '\\' and not '\n')
+                    {
+                        currentString += ch;
+                    }
+
+                    lastSymbol = ch;
+                    break;
+            }
+        }
+
+        // File ended without finishing a special character
+        if (specialCharacterState != SpecialCharacterState.None)
+        {
+            ch = ' ';
+            HandleSpecialCharacter();
+        }
+
+        switch (state)
+        {
+            case State.String:
+                res.Strings[currentIdsNumber] = currentString;
+                break;
+            case State.Info:
+                res.Infocards[currentIdsNumber] = InfocardStart + currentString + InfocardEnd;
+                break;
+            default:
+                throw new CompileErrorException(source, reader.Column, reader.Line,
+                    $"State was invalid at file end");
+        }
+
+        return res;
+
+        bool HandleSpecialCharacter()
+        {
+            if (specialCharacterState is SpecialCharacterState.None)
+            {
+                switch (ch)
+                {
+                    case '\\':
+                        currentString += '\\';
+                        break;
+                    case 'b':
+                        currentString += """<TRA bold="true"/>""";
+                        break;
+                    case 'B':
+                        currentString += """<TRA bold="false"/>""";
+                        break;
+                    case 'c':
+                        specialCharacterState = SpecialCharacterState.Color;
+                        return false;
+                    case 'C':
+                        currentString += """<TRA color="default"/>""";
+                        break;
+                    case 'f':
+                        specialCharacterState = SpecialCharacterState.FontNumber;
+                        return false;
+                    case 'F':
+                        currentString += """<TRA font="default"/>""";
+                        break;
+                    case 'h':
+                        specialCharacterState = SpecialCharacterState.Pos;
+                        return false;
+                    case 'i':
+                        currentString += """<TRA italic="true"/>""";
+                        break;
+                    case 'I':
+                        currentString += """<TRA italic="false"/>""";
+                        break;
+                    case 'l':
+                        currentString += """<JUST loc="l"/>""";
+                        break;
+                    case 'm':
+                        currentString += """<JUST loc="c"/>""";
+                        break;
+                    case 'n':
+                        currentString += state is State.Info ? "<PARA/>" : "\n";
+                        break;
+                    case 'r':
+                        currentString += """<JUST loc="r"/>""";
+                        break;
+                    case 'u':
+                        currentString += """<TRA underline="true"/>""";
+                        break;
+                    case 'U':
+                        currentString += """<TRA underline="false"/>""";
+                        break;
+                    case 'x':
+                        specialCharacterState = SpecialCharacterState.Unicode;
+                        return false;
+                    case '.':
+                        break;
+                    default:
+                    {
+                        throw new CompileErrorException(source, reader.Column, reader.Line,
+                            $"Unexpected character '{ch}'. Expected special character.");
+                    }
+                }
+
+                lastSymbol = ch;
+                return false;
+            }
+
+            switch (specialCharacterState)
+            {
+                case SpecialCharacterState.Color:
+                {
+                    if (ch is not ' ')
+                    {
+                        currentSpecialString += ch;
+                        return false;
+                    }
+
+                    if (Enum.TryParse<AvailableColour>(currentSpecialString, out var color))
+                    {
+                        currentString += $"""<TRA color="{color.ToString().ToLowerInvariant()}"/>""";
+                    }
+                    else if (currentSpecialString.Length == 6)
+                    {
+                        if (!currentSpecialString.All(char.IsAsciiHexDigit))
+                        {
+                            throw new CompileErrorException(source, reader.Column, reader.Line,
+                                $"Provided colour was not a valid RRGGBB hex string.");
+                        }
+
+                        currentString += $"""<TRA color="#{currentSpecialString.ToUpper()}"/>""";
+                    }
+                    else
+                    {
+                        throw new CompileErrorException(source, reader.Column, reader.Line,
+                            $"Provided colour was not a valid colour string.");
+                    }
+
+                    break;
+                }
+                case SpecialCharacterState.FontNumber:
+                    if (ch is not ' ')
+                    {
+                        currentSpecialString += ch;
+                        return false;
+                    }
+
+                    if (int.TryParse(currentSpecialString, out var fontNumber) && fontNumber is > 0 and < 100)
+                    {
+                        currentString += $"""<TRA font="{fontNumber}"/>""";
+                    }
+                    else
+                    {
+                        throw new CompileErrorException(source, reader.Column, reader.Line,
+                            $"Provided font was not a valid number. Should be between 1-99 (inclusive).");
+                    }
+
+                    break;
+                case SpecialCharacterState.Pos:
+                    if (ch is not ' ')
+                    {
+                        currentSpecialString += ch;
+                        return false;
+                    }
+
+                    if (int.TryParse(currentSpecialString, out var pos) && pos is > 0 and < 1000)
+                    {
+                        currentString += $"""<POS h="{pos}" relH="true"/>""";
+                    }
+                    else
+                    {
+                        throw new CompileErrorException(source, reader.Column, reader.Line,
+                            $"Provided pos was not a valid number. Should be between 1-999 (inclusive).");
+                    }
+
+                    break;
+                case SpecialCharacterState.Unicode:
+                    if (currentSpecialString.Length is not 4 && ch is not ' ')
+                    {
+                        currentSpecialString += ch;
+                        return false;
+                    }
+
+                    if (currentSpecialString.Length is not 4)
+                    {
+                        throw new CompileErrorException(source, reader.Column, reader.Line,
+                            $"Provided unicode string was not four characters.");
+                    }
+
+                    if (!currentSpecialString.All(char.IsAsciiHexDigit))
+                    {
+                        throw new CompileErrorException(source, reader.Column, reader.Line,
+                            $"Provided unicode was not a valid hex string.");
+                    }
+
+                    var hex = Convert.ToInt32(currentSpecialString, 16);
+                    currentString += char.ConvertFromUtf32(hex);
+                    break;
+            }
+
+            currentSpecialString = string.Empty;
+            specialCharacterState = SpecialCharacterState.None;
+            lastSymbol = ch;
+            return true;
+        }
+
+        bool HandleIds()
+        {
+            if (currentString.Length is 0 && IsSpace(ch))
+            {
+                return false;
+            }
+
+            // Set the IDS number and start reading text
+            if (char.IsNumber(ch))
+            {
+                currentString += ch.ToString();
+                lastSymbol = ch;
+                return false;
+            }
+
+            if (currentString.Length is 0)
+            {
+                throw new CompileErrorException(source, reader.Column, reader.Line,
+                    $"Unexpected character '{ch}'. Expected number or whitespace.");
+            }
+
+            currentIdsNumber = int.Parse(currentString);
+            currentString = "";
+
+            return true;
+        }
+
+        bool Advance(bool skip)
+        {
+            reader.Advance();
+
+            if (!skip)
+            {
+                return reader.Current() != -1;
+            }
+
+            int character;
+            while ((character = reader.Current()) != -1 && IsSpace((char)character))
+            {
+                reader.Advance();
+            }
+
+            return (character != -1);
+
+        }
+    }
+}
---
+++ src/LibreLancer.Tests/Compilers/FrcTests.cs
@@ -0,0 +1,124 @@
+using System.IO;
+using LibreLancer.ContentEdit.Frc;
+using LibreLancer.ContentEdit.RandomMissions;
+using Xunit;
+
+namespace LibreLancer.Tests.Compilers;
+
+public class FrcTests
+{
+    [Fact]
+    public void ValidFrcStringsShouldReturnAValidResourceDll()
+    {
+        const string fileName = "Compilers/FrcFiles/ValidFrcStrings.frc";
+        var strings = File.ReadAllText(fileName);
+
+        FrcCompiler compiler = new FrcCompiler();
+        var resourceDll = compiler.Compile(strings, fileName);
+
+        Assert.NotNull(resourceDll);
+
+        Assert.Equal(expected: "File Starts Here", actual: resourceDll.Strings[1]);
+        Assert.Equal(expected: "MultiLine\nString", actual: resourceDll.Strings[2]);
+        Assert.Equal(expected: "Two\nLines", actual: resourceDll.Strings[3]);
+        Assert.Equal(expected: "One Line", actual: resourceDll.Strings[4]);
+        Assert.Equal(expected: " Spacing ", actual: resourceDll.Strings[5]);
+        Assert.Equal(expected: "Separate Line", actual: resourceDll.Strings[6]);
+        Assert.Equal(expected: "“Read My Quote”", actual: resourceDll.Strings[7]);
+    }
+
+    [Fact]
+    public void ValidFrcInfocardsShouldReturnAValidResourceDll()
+    {
+        const string fileName = "Compilers/FrcFiles/ValidFrcInfocards.frc";
+        var strings = File.ReadAllText(fileName);
+
+        FrcCompiler compiler = new FrcCompiler();
+        var resourceDll = compiler.Compile(strings, fileName);
+
+        Assert.NotNull(resourceDll);
+
+        Assert.Equal(expected: WrapInfocard("<TRA bold=\"true\"/>Bold <TRA bold=\"false\"/>Not Bold"),
+            actual: resourceDll.Infocards[1]);
+        Assert.Equal(expected: WrapInfocard("MultiLine<PARA/>String"), actual: resourceDll.Infocards[2]);
+        Assert.Equal(expected: WrapInfocard("Two<PARA/>Lines"), actual: resourceDll.Infocards[3]);
+        Assert.Equal(expected: WrapInfocard("One Line"), actual: resourceDll.Infocards[4]);
+        Assert.Equal(expected: WrapInfocard(" Spacing "), actual: resourceDll.Infocards[5]);
+        Assert.Equal(expected: WrapInfocard("Separate Line"), actual: resourceDll.Infocards[6]);
+        Assert.Equal(expected: WrapInfocard("“Read My Quote”"), actual: resourceDll.Infocards[7]);
+    }
+
+    [Theory]
+    [MemberData(nameof(ValidInfocardFrcStrings))]
+    public void ValidFrcStringsShouldTransformToTheCorrectXml(string input, string expected)
+    {
+        FrcCompiler compiler = new FrcCompiler();
+        var resourceDll = compiler.Compile(input, "TEST");
+
+        Assert.NotNull(resourceDll);
+        Assert.Equal(expected: WrapInfocard(expected), actual: resourceDll.Infocards[1]);
+    }
+
+    [Theory]
+    [InlineData("white")]
+    [InlineData("ReD")]
+    [InlineData("Black")]
+    [InlineData("DarkBlue")]
+    [InlineData("ABCABCA")] // Bad Hex
+    public void BadColoursShouldThrowExceptions(string colour)
+    {
+        FrcCompiler compiler = new FrcCompiler();
+        Assert.Throws<CompileErrorException>(() =>
+        {
+            _ = compiler.Compile($"I 1 \\c{colour}", "TEST");
+        });
+    }
+
+    [Theory]
+    [InlineData("1000")]
+    [InlineData("-12")]
+    [InlineData("0")]
+    public void BadHeightShouldThrowExceptions(string height)
+    {
+        FrcCompiler compiler = new FrcCompiler();
+        Assert.Throws<CompileErrorException>(() =>
+        {
+            _ = compiler.Compile($"I 1 \\h{height}", "TEST");
+        });
+    }
+
+    [Theory]
+    [InlineData("100")]
+    [InlineData("-12")]
+    [InlineData("0")]
+    public void BadFontShouldThrowExceptions(string font)
+    {
+        FrcCompiler compiler = new FrcCompiler();
+        Assert.Throws<CompileErrorException>(() => { _ = compiler.Compile($"I 1 \\f{font}", "TEST"); });
+    }
+
+
+    public static TheoryData<string, string> ValidInfocardFrcStrings { get; } = new()
+    {
+        { @"I 1 \b", "<TRA bold=\"true\"/>" },
+        { @"I 1 \B", "<TRA bold=\"false\"/>" },
+        { @"I 1 \f12", "<TRA font=\"12\"/>" },
+        { @"I 1 \F", "<TRA font=\"default\"/>" },
+        { @"I 1 \i", "<TRA italic=\"true\"/>" },
+        { @"I 1 \I", "<TRA italic=\"false\"/>" },
+        { @"I 1 \u", "<TRA underline=\"true\"/>" },
+        { @"I 1 \U", "<TRA underline=\"false\"/>" },
+        { @"I 1 \n", "<PARA/>" },
+        { @"I 1 \r", "<JUST loc=\"r\"/>" },
+        { @"I 1 \m", "<JUST loc=\"c\"/>" },
+        { @"I 1 \l", "<JUST loc=\"l\"/>" },
+        { @"I 1 \h150", "<POS h=\"150\" relH=\"true\"/>" },
+        { @"I 1 \C", "<TRA color=\"default\"/>" },
+        { @"I 1 \cWhite", "<TRA color=\"white\"/>" },
+        { @"I 1 \cFFFFFF", "<TRA color=\"#FFFFFF\"/>" },
+    };
+
+
+
+    private string WrapInfocard(string infocard) => $"{FrcCompiler.InfocardStart}{infocard}{FrcCompiler.InfocardEnd}";
+}
