d61af9a0ed159654200a494e385ea78a513f23ea
CallumDev <callum.mcging@gmail.com>
2025-04-25 21:59:55 +0930

    SurfaceBuilder: Increase tree build speed dramatically, fix "Root" rename not applying.

--- src/Editor/LibreLancer.ContentEdit/Extensions.cs
+++ src/Editor/LibreLancer.ContentEdit/Extensions.cs
@@ -8,13 +8,13 @@ namespace LibreLancer.ContentEdit
 {
     static class Extensions
     {
-        
+
         public static StringBuilder AppendFmtLine(this StringBuilder builder, object o)
             => builder.AppendLine(o.ToString());
 
         public static bool AddIfUnique<T>(this List<T> list, T item)
             => AddIfUnique(list, item, out _);
-        
+
         public static bool AddIfUnique<T>(this List<T> list, T item, out int index)
         {
             index = list.IndexOf(item);
@@ -25,12 +25,5 @@ namespace LibreLancer.ContentEdit
             }
             return false;
         }
-
-        public static int IndexOfMin<T>(this IEnumerable<T> self)
-            => self.Select((item, index) => (item, index)).Min().index;
-        
-        public static int IndexOfMax<T>(this IEnumerable<T> self)
-            => self.Select((item, index) => (item, index)).Max().index;
-        
     }
 }
