9e58c91ee74b08105d883007ae7511a58d66761b
George Chambers <georgeharsent@gmail.com>
2025-12-12 01:07:09 +0000

    Implemented ImportEntry
    Stores data related to items to be imported - this can then be passed upstream to the utf to create the required nodes

---
+++ src/Editor/LancerEdit/Tools/BulkAudio/ImportEntry.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LancerEdit.Tools.BulkAudio
+{
+    public class ImportEntry
+    {
+        public string FileName;       
+        public string NodeName;       
+        public byte[] Data;           
+        public ImportVersion Version;
+        public ImportAction Action;
+    }
+
+    public enum ImportAction
+    {
+        Ignore,
+        Import
+    }
+
+    public enum ImportVersion
+    {
+        Original,
+        Converted
+    }
+}
