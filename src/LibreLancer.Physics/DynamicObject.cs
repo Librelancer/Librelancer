0386238ee1d816d0c05cb1502b76f508ea3b99a0
Josbyte <josemiguelsanchezarje@gmail.com>
2025-12-12 16:25:11 +0100

    fixes V3

--- src/LibreLancer.Physics/DynamicObject.cs
+++ src/LibreLancer.Physics/DynamicObject.cs
@@ -18,98 +18,97 @@ internal class DynamicObject : PhysicsObject
 
     private bool IsValid => BepuObject.Bodies.BodyExists(BepuObject.Handle);
 
+    private void CheckValid()
+    {
+        if (!IsValid)
+            throw new ObjectDisposedException(nameof(DynamicObject), "The physics object is no longer valid.");
+    }
+
     public override bool Collidable
     {
-        get => IsValid && world.collidableObjects[BepuObject.Handle];
-        set { if (IsValid) world.collidableObjects[BepuObject.Handle] = value; }
+        get { CheckValid(); return world.collidableObjects[BepuObject.Handle]; }
+        set { CheckValid(); world.collidableObjects[BepuObject.Handle] = value; }
     }
 
     public override bool Static => false;
-    public override bool Active => IsValid && BepuObject.Awake;
+    public override bool Active { get { CheckValid(); return BepuObject.Awake; } }
 
     public override Vector3 Position
     {
-        get => IsValid ? BepuObject.Pose.Position : Vector3.Zero;
-        protected set { if (IsValid) BepuObject.Pose.Position = value; }
+        get { CheckValid(); return BepuObject.Pose.Position; }
+        protected set { CheckValid(); BepuObject.Pose.Position = value; }
     }
 
     public override Quaternion Orientation
     {
-        get => IsValid ? BepuObject.Pose.Orientation : Quaternion.Identity;
-        protected set { if (IsValid) BepuObject.Pose.Orientation = value; }
+        get { CheckValid(); return BepuObject.Pose.Orientation; }
+        protected set { CheckValid(); BepuObject.Pose.Orientation = value; }
     }
 
     public override void SetTransform(Transform3D transform)
     {
-        if (IsValid)
-        {
-            Position = transform.Position;
-            Orientation = transform.Orientation;
-            BepuObject.UpdateBounds();
-        }
+        CheckValid();
+        Position = transform.Position;
+        Orientation = transform.Orientation;
+        BepuObject.UpdateBounds();
     }
 
     public override void SetOrientation(Quaternion orientation)
     {
-        if (IsValid)
-        {
-            BepuObject.Pose.Orientation = orientation;
-            BepuObject.UpdateBounds();
-        }
+        CheckValid();
+        BepuObject.Pose.Orientation = orientation;
+        BepuObject.UpdateBounds();
     }
 
     public override Vector3 AngularVelocity
     {
-        get => IsValid ? BepuObject.Velocity.Angular : Vector3.Zero;
+        get { CheckValid(); return BepuObject.Velocity.Angular; }
         set
         {
-            if (IsValid)
-            {
-                BepuObject.Velocity.Angular = value;
-                if (value.LengthSquared() > 0)
-                    BepuObject.Awake = true;
-            }
+            CheckValid();
+            BepuObject.Velocity.Angular = value;
+            if (value.LengthSquared() > 0)
+                BepuObject.Awake = true;
         }
     }
 
     public override Vector3 LinearVelocity
     {
-        get => IsValid ? BepuObject.Velocity.Linear : Vector3.Zero;
+        get { CheckValid(); return BepuObject.Velocity.Linear; }
         set
         {
-            if (IsValid)
-            {
-                BepuObject.Velocity.Linear = value;
-                if (value.LengthSquared() > 0)
-                    BepuObject.Awake = true;
-            }
+            CheckValid();
+            BepuObject.Velocity.Linear = value;
+            if (value.LengthSquared() > 0)
+                BepuObject.Awake = true;
         }
     }
 
     public override BoundingBox GetBoundingBox()
     {
-        if (!IsValid)
-            throw new ObjectDisposedException(nameof(DynamicObject), "The physics object is no longer valid.");
+        CheckValid();
         var bounds = BepuObject.BoundingBox;
         return new BoundingBox(bounds.Min, bounds.Max);
     }
 
     public override Vector3 RotateVector(Vector3 src)
     {
+        CheckValid();
         return Vector3.Transform(src, BepuObject.Pose.Orientation);
     }
 
     public override void SetDamping(float linearDamping, float angularDamping)
     {
-        if (IsValid)
-            world.dampings[BepuObject.Handle] = new Vector2(linearDamping, angularDamping);
+        CheckValid();
+        world.dampings[BepuObject.Handle] = new Vector2(linearDamping, angularDamping);
     }
 
     private const float ForceFactor = (1 / 60.0f);
 
     public override void AddForce(Vector3 force)
     {
-        if (IsValid && force.LengthSquared() > 0)
+        CheckValid();
+        if (force.LengthSquared() > 0)
         {
             BepuObject.ApplyImpulse(force * ForceFactor, Vector3.Zero);
             BepuObject.Awake = true;
@@ -118,13 +117,14 @@ internal class DynamicObject : PhysicsObject
 
     public override void Activate()
     {
-        if (IsValid)
-            BepuObject.Awake = true;
+        CheckValid();
+        BepuObject.Awake = true;
     }
 
     public override void Impulse(Vector3 force)
     {
-        if (IsValid && force.LengthSquared() > 0)
+        CheckValid();
+        if (force.LengthSquared() > 0)
         {
             BepuObject.ApplyImpulse(force, Vector3.Zero);
             BepuObject.Awake = true;
@@ -133,7 +133,8 @@ internal class DynamicObject : PhysicsObject
 
     public override void AddTorque(Vector3 torque)
     {
-        if (IsValid && torque.LengthSquared() > 0)
+        CheckValid();
+        if (torque.LengthSquared() > 0)
         {
             BepuObject.ApplyAngularImpulse(torque * ForceFactor);
             BepuObject.Awake = true;
@@ -142,11 +143,9 @@ internal class DynamicObject : PhysicsObject
 
     public override void PredictionStep(float timestep)
     {
-        if (IsValid)
-        {
-            PoseIntegration.Integrate(BepuObject.Pose, BepuObject.Velocity, timestep, out BepuObject.Pose);
-            UpdateProperties();
-        }
+        CheckValid();
+        PoseIntegration.Integrate(BepuObject.Pose, BepuObject.Velocity, timestep, out BepuObject.Pose);
+        UpdateProperties();
     }
 
     internal override void UpdateProperties()
