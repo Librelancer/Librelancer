3bb2275f858916b8544b3f1ef2e66584141778ce
George Chambers <georgeharsent@gmail.com>
2025-12-14 05:32:58 +0000

    migrated all required functionality to new screen ServerConfigScreen
    fixed layout so fields scale
    Added folder/file select buttons for FL folder, database file and config file

--- src/LLServerGui/Screens/ServerConfigurationScreen.cs
+++ src/LLServerGui/Screens/ServerConfigurationScreen.cs
@@ -1,36 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Numerics;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using ImGuiNET;
 using LibreLancer;
 using LibreLancer.Data;
+using LibreLancer.Dialogs;
 using LibreLancer.ImUI;
+using LibreLancer.Server.Ai;
 using Microsoft.Win32;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace LLServer.Screens;
 
 
 public class ServerConfigurationScreen : Screen
 {
-    public ServerConfigurationScreen(MainWindow window, ScreenManager screens, PopupManager popups) : base(screens, popups)
+    public ServerConfigurationScreen(MainWindow win, ScreenManager sm, PopupManager pm) : base(sm, pm)
     {
-        this.window = window;
+        this.win = win;
 
-        configPath = Path.Combine(Platform.GetBasePath(), "llserver.json");
+        win.ConfigPath = Path.Combine(Platform.GetBasePath(), "llserver.json");
     }
 
-    private readonly MainWindow window;
+    static readonly FileDialogFilters dbInputFilters = new FileDialogFilters(
+        new FileFilter("Database File", "db")
+        );
+    static readonly FileDialogFilters inputFilters = new FileDialogFilters(
+        new FileFilter("Config File", "json")
+        );
 
-    private ServerConfig config;
-    private string configPath;
-    private bool startupError;
+    static readonly float LABEL_WIDTH = 125f;
+    static readonly float BUTTON_WIDTH = 110f;
+    readonly Vector4 ERROR_TEXT_COLOUR = new Vector4(1f, 0.3f, 0.3f, 1f);
+    readonly Vector4 SUCCESS_TEXT_COLOUR = new Vector4(0f, 0.8f, 0.2f, 1f);
+
+    readonly MainWindow win;
+    ServerConfig config;
 
     public override void OnEnter()
     {
         config = GetConfigFromFileOrDefault();
-        startupError = false;
+        win.StartupError = false;
 
         base.OnEnter();
     }
@@ -38,54 +52,116 @@ public class ServerConfigurationScreen : Screen
     public override void Draw(double elapsed)
     {
         ImGui.PushFont(ImGuiHelper.Roboto, 32);
-        ImGui.Text("Server Configuration");
+        CenterText("Server Configuration");
         ImGui.PopFont();
 
         ImGui.NewLine();
-        ImGui.PushItemWidth(540);
+        ImGui.AlignTextToFramePadding();
+        ImGui.Text("Server Name"); ImGui.SameLine(LABEL_WIDTH * ImGuiHelper.Scale);
+        ImGui.PushItemWidth(-1); ImGui.InputText("##serverName", ref config.ServerName, 4096);
 
-        InputTextLabel("Server Name", "##serverName", ref config.ServerName);
-
-        ImGui.Text("Server Description");
+        ImGui.AlignTextToFramePadding();
+        ImGui.Text("Server Description"); ImGui.SameLine(LABEL_WIDTH * ImGuiHelper.Scale);
+        ImGui.PushItemWidth(-1);
         ImGui.InputTextMultiline(
             "##description",
             ref config.ServerDescription,
             4096,
-            ImGui.GetContentRegionAvail()
+            new Vector2(0, ImGui.GetFrameHeight() * 4 * ImGuiHelper.Scale)
         );
 
-        InputTextLabel("Freelancer Path", "##flpath", ref config.FreelancerPath);
-        InputTextLabel("Database File", "##dbfile", ref config.DatabasePath);
-        InputTextLabel("Configuration File", "##configfile", ref configPath);
-
-        ImGui.PopItemWidth();
-        ImGui.NewLine();
-
-        if (startupError)
+        ImGui.AlignTextToFramePadding();
+        ImGui.Text("Freelancer Path"); ImGui.SameLine(LABEL_WIDTH * ImGuiHelper.Scale);
+        if (ImGui.Button("Select Folder", new Vector2(BUTTON_WIDTH * ImGuiHelper.Scale, 0)))
         {
-            ImGui.TextColored(
-                new System.Numerics.Vector4(1, 0, 0, 1),
-                "Server startup failed"
-            );
+            win.QueueUIThread(() =>
+            {
+                FileDialog.ChooseFolder(folder =>
+                {
+                    if (folder == null || folder.Length == 0)
+                    {
+                        return;
+                    }
+                    config.FreelancerPath = folder;
+
+                });
+            });
+        }
+        ImGui.SameLine(); ImGui.PushItemWidth(-1); ImGui.InputText("##flpath", ref config.FreelancerPath, 4096);
+
+        ImGui.AlignTextToFramePadding();
+        ImGui.Text("Database File"); ImGui.SameLine(LABEL_WIDTH * ImGuiHelper.Scale);
+        if (ImGui.Button("Select File##db", new Vector2(BUTTON_WIDTH * ImGuiHelper.Scale, 0)))
+        {
+            win.QueueUIThread(() =>
+            {
+                FileDialog.Open(filepath =>
+                {
+                    if (filepath == null || filepath.Length == 0)
+                    {
+                        return;
+                    }
+                    config.FreelancerPath = filepath;
+
+                },
+                dbInputFilters);
+            });
+        }
+        ImGui.SameLine(); ImGui.PushItemWidth(-1); ImGui.InputText("##dbfile", ref config.DatabasePath, 4096);
+
+        ImGui.AlignTextToFramePadding();
+        ImGui.Text("Configuration File"); ImGui.SameLine(LABEL_WIDTH * ImGuiHelper.Scale);
+        if (ImGui.Button("Select File##config", new Vector2(BUTTON_WIDTH * ImGuiHelper.Scale, 0)))
+        {
+            win.QueueUIThread(() =>
+            {
+                FileDialog.Open(filepath =>
+                {
+                    if (filepath == null || filepath.Length == 0)
+                    {
+                        return;
+                    }
+                    config.FreelancerPath = filepath;
+                },
+                inputFilters);
+            });
+        }
+        ImGui.SameLine(); ImGui.PushItemWidth(-1); ImGui.InputText("##configfile", ref win.ConfigPath, 4096);
+
+        ImGui.Spacing();
+        ImGui.Separator();
+        ImGui.Spacing();
+
+        if (ImGui.Button("Launch Server", new Vector2(BUTTON_WIDTH * ImGuiHelper.Scale, 0)))
+        {
+            win.QueueUIThread(() =>
+            {
+                LaunchServer();
+            });
+        }
+        ImGui.Spacing();
+        ImGui.Separator();
+        ImGui.Spacing();
+        if (win.StartupError)
+        {
+            ImGui.Dummy(new Vector2(0, ImGui.GetContentRegionAvail().Y - ImGui.GetFrameHeightWithSpacing() - 5 * ImGuiHelper.Scale));
+            ImGui.BeginChild("startupError", new Vector2(0, ImGui.GetFrameHeightWithSpacing() * ImGuiHelper.Scale), ImGuiChildFlags.None, ImGuiWindowFlags.NoScrollbar);
+            CenterText("Server Startup Error", ERROR_TEXT_COLOUR);
+            ImGui.EndChild();
         }
 
-        if (ImGui.Button("Launch"))
-        {
-            LaunchServer();
-        }
     }
 
     public override void OnExit()
     {
-        base.OnExit();
     }
 
     ServerConfig GetConfigFromFileOrDefault()
     {
         ServerConfig config = new ServerConfig();
 
-        if (File.Exists(configPath))
-            config = JSON.Deserialize<ServerConfig>(File.ReadAllText(configPath));
+        if (File.Exists(win.ConfigPath))
+            config = JSON.Deserialize<ServerConfig>(File.ReadAllText(win.ConfigPath));
         else
         {
             config = new ServerConfig();
@@ -117,29 +193,32 @@ public class ServerConfigurationScreen : Screen
     {
         Task.Run(() =>
         {
-            if (!window.StartServer(config))
-            {
-                window.QueueUIThread(() => startupError = true);
-            }
-            else
-            {
-                File.WriteAllText(configPath, JSON.Serialize(config));
+            File.WriteAllText(win.ConfigPath, JSON.Serialize(config));
 
-                window.QueueUIThread(() =>
-                {
-                    Screens.SetScreen(
-                        new RunningServerScreen(window, Screens, Popups)
-                    );
-                });
-            }
+            win.QueueUIThread(() =>
+            {
+                Screens.SetScreen(
+                    new RunningServerScreen(win, Screens, Popups, config)
+                );
+            });
         });
     }
 
-    void InputTextLabel(string label, string id, ref string text)
+    void CenterText(string text)
     {
-        ImGui.AlignTextToFramePadding();
-        ImGui.Text(label);
-        ImGui.InputText(id, ref text, 4096);
+        ImGui.Dummy(new Vector2(1));
+        var win = ImGui.GetWindowWidth();
+        var txt = ImGui.CalcTextSize(text).X;
+        ImGui.SameLine(Math.Max((win / 2f) - (txt / 2f), 0));
+        ImGui.Text(text);
     }
+    void CenterText(string text, Vector4 colour)
+    {
+        ImGui.Dummy(new Vector2(1));
+        var win = ImGui.GetWindowWidth();
+        var txt = ImGui.CalcTextSize(text).X;
+        ImGui.SameLine(Math.Max((win / 2f) - (txt / 2f), 0));
+        ImGui.TextColored(ERROR_TEXT_COLOUR, text);
 
+    }
 }
