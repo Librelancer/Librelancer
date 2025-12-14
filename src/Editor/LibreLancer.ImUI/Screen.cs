b43f339dd7a4201c77483f65e3aed28ffc613ddd
George Chambers <georgeharsent@gmail.com>
2025-12-14 01:13:07 +0000

    Added A screen base class to encapsulate different screens
    Added a screen Manager to manage transitioning between different screens

---
+++ src/Editor/LibreLancer.ImUI/Screen.cs
@@ -0,0 +1,43 @@
+﻿using ImGuiNET;
+
+namespace LibreLancer.ImUI
+{
+    /// <summary>
+    /// Base class for all ImGui screens.
+    /// Screens represent mutually-exclusive UI states.
+    /// </summary>
+    public abstract class Screen
+    {
+        protected readonly ScreenManager Screens;
+        protected readonly PopupManager Popups;
+
+        protected Screen(ScreenManager screens, PopupManager popups)
+        {
+            Screens = screens;
+            Popups = popups;
+        }
+
+        /// <summary>
+        /// Called once when the screen becomes active.
+        /// Use for initialization and setup.
+        /// </summary>
+        public virtual void OnEnter()
+        {
+        }
+
+        /// <summary>
+        /// Called once when the screen is removed.
+        /// Use for final cleanup only.
+        /// </summary>
+        public virtual void OnExit()
+        {
+        }
+
+        /// <summary>
+        /// Called every frame to render the screen.
+        /// All user decisions must happen here.
+        /// </summary>
+        public abstract void Draw(double elapsed);
+
+    }
+}
