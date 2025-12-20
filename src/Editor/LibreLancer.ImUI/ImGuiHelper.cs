0170ce82ca62aad5a3371055f718962840c6ee99
George Chambers <georgeharsent@gmail.com>
2025-12-20 08:09:09 +0000

    revert change

--- src/Editor/LibreLancer.ImUI/ImGuiHelper.cs
+++ src/Editor/LibreLancer.ImUI/ImGuiHelper.cs
@@ -382,12 +382,10 @@ namespace LibreLancer.ImUI
         public static void AnimatingElement() => animating = true;
         public static void DeregisterTexture(Texture2D tex)
 		{
-            if(textureIds.ContainsKey(tex)){
-			    var id = textureIds[tex];
-			    textureIds.Remove(tex);
-			    textures.Remove(id);
-			    freeIds.Enqueue(id);
-            }
+		    var id = textureIds[tex];
+		    textureIds.Remove(tex);
+		    textures.Remove(id);
+		    freeIds.Enqueue(id);
 		}
 
 		void Keyboard_TextInput(string text)
