450380aef594f28e4dad6bb25e535613ea75e424
George Chambers <georgeharsent@gmail.com>
2025-12-27 12:24:23 +0000

    Generic classes for SystemObjectGroups to help define different groups down the line

---
+++ src/Editor/LancerEdit/GameContent/Groups/SystemObjectGroup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LancerEdit.GameContent.Groups;
+
+public abstract class SystemObjectGroup<T>
+{
+    public readonly List<T> Members = new();
+
+    public bool Contains(T obj) => Members.Contains(obj);
+
+    public virtual void Add(T obj)
+    {
+        if (!Members.Contains(obj))
+            Members.Add(obj);
+    }
+}
