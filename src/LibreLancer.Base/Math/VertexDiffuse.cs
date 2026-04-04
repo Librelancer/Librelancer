0615a8b7e83312c3d33b2bb98c76a453a2759395
CallumDev <callum.mcging@gmail.com>
2026-04-05 02:38:44 +0930

    VertexDiffuse: Use SIMD for conversion from Color4/Vector4

--- src/LibreLancer.Base/Math/VertexDiffuse.cs
+++ src/LibreLancer.Base/Math/VertexDiffuse.cs
@@ -1,5 +1,9 @@
 using System.Numerics;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Runtime.Intrinsics;
+using System.Runtime.Intrinsics.Arm;
+using System.Runtime.Intrinsics.X86;
 
 namespace LibreLancer;
 
@@ -25,19 +29,24 @@ public struct VertexDiffuse(byte r, byte g, byte b, byte a)
 
     public static implicit operator uint(VertexDiffuse diffuse) => diffuse.Pixel;
 
-    public static explicit operator VertexDiffuse(Color4 color)
+    public static explicit operator VertexDiffuse(Vector4 color)
     {
-        var a = (byte) MathHelper.Clamp(color.A * 255, 0, 255);
-        var r = (byte)MathHelper.Clamp(color.R * 255, 0, 255);
-        var g = (byte)MathHelper.Clamp(color.G * 255, 0, 255);
-        var b = (byte)MathHelper.Clamp(color.B * 255, 0, 255);
-        return new VertexDiffuse(r, g, b, a);
+        var clamped = Vector128.MinNative(
+            Vector128.MaxNative((color * 255).AsVector128(), Vector128<float>.Zero),
+            Vector128.Create<float>(255));
+        var ints = Vector128.ConvertToInt32Native(clamped);
+        var shorts = Vector128.Narrow(ints, ints);
+        var bytes = Vector128.Narrow(shorts, shorts)
+            .AsUInt32();
+        return (VertexDiffuse)bytes.ToScalar();
     }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static explicit operator VertexDiffuse(Color4 color) =>
+        (VertexDiffuse)Unsafe.BitCast<Color4, Vector4>(color);
 
     public static explicit operator Color4(VertexDiffuse diffuse) =>
         new Color4(diffuse.R / 255f, diffuse.G / 255f, diffuse.B / 255f, diffuse.A / 255f);
-
-    public static explicit operator VertexDiffuse(Vector4 color) => (VertexDiffuse)(Color4)color;
 
     public static explicit operator VertexDiffuse(uint source)
     {
